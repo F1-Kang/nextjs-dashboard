f40a622c682923b8d8b81076da1f17942c145a1e
F1-Kang <hoangnito@thousandsunny.com.vn>
2026-01-08 19:03:39 +0700

    chapter 1-11 complete

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,12 +16,12 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
+    // console.log('Đang tìm nạp dữ liệu doanh thu...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    // console.log('Việc tìm nạp dữ liệu hoàn tất sau 3 giây.');
 
     return data;
   } catch (error) {
